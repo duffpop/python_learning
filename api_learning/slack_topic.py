fe753132d3b245cf2f6e7627c7fbe5bf8cdcdfa0
Hayden <hayden.duffy@depop.com>
2020-06-18 21:29:41 +0100

    adding @username for DMing the shift member

--- api_learning/slack_topic.py
+++ api_learning/slack_topic.py
@@ -11,13 +11,28 @@ BASEDIR = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(BASEDIR, '.env'))
 
 # Slack set topic URL
-url = 'https://slack.com/api/conversations.setTopic'
+topic_url = 'https://slack.com/api/conversations.setTopic'
+# Slack post message URL
+post_message_url = 'https://slack.com/api/chat.postMessage'
+# Slack token
 slack_token = os.environ.get('SLACK_TOKEN')
 # #it-support channel
 slack_channel = 'G0125J6V866'
 
 # list of IT Support members
 it_list = ['hayden', 'adeel', 'alex']
+
+it_dict = {
+    'hayden': ['DEPF8CW2G', 'UEPH6G519'],
+    'adeel': ['DHNT8FJ8G', 'UHNT8DGGY'],
+    'alex': ['D0123FPLCE9', 'U011VK4K44S']
+}
+
+mention_dict = {
+    'hayden': 'UEPH6G519',
+    'adeel': 'UHNT8DGGY',
+    'alex': 'U011VK4K44S'
+}
 
 # txt file which stores the schedule for the week
 schedule_file = 'weekly_schedule.txt'
@@ -45,15 +60,17 @@ def fill_schedule():
     # Fills the weekly_list schedule and calls the max_shifts function to ensure
     # that no IT Member is placed on more than 2 shifts a week
     while len(weekly_list) != 5:
-        weekly_list.append(choice(it_list))
+        weekly_list.append(choice(list(it_dict.keys())))
         max_shifts(weekly_list, 2)
 
 
 def write_schedule(today):
+    # If the day is Sunday, the schedule gets wiped
     if today == 'Sunday':
         with open(schedule_file, 'w') as file_object:
             for element in weekly_list:
                 file_object.write(element + '\n')
+        del weekly_list[:]
     else:
         print('Not re-writing schedule today.')
 
@@ -64,7 +81,7 @@ def read_schedule():
         for line in lines:
             weekly_list.append(line.rstrip())
             # print(line)
-    print(weekly_list)
+    # print(weekly_list)
 
 
 def compare_day_schedule():
@@ -78,10 +95,37 @@ def compare_day_schedule():
             continue
 
 
+def get_shift_member_channel_id():
+    name = f'{compare_day_schedule()}'
+    # print(name)
+    for key, value in it_dict.items():
+        if name == key:
+            # print(f'{key} + {value}')
+            shift_member_id = value[0]
+            return shift_member_id
+        else:
+            continue
+
+
+def get_shift_member_user_id():
+    name = f'{compare_day_schedule()}'
+    # print(name)
+    for key, value in it_dict.items():
+        if name == key:
+            # print(f'{key} + {value}')
+            shift_member_id = value[1]
+            return shift_member_id
+        else:
+            continue
+
+
 def set_topic():
     # Post to Slack with the below topic, data to the channel above
-    r = requests.post(url, data=data)
-    print(r.status_code)
+    topic_post = requests.post(topic_url, data=topic_data)
+    print(f'Topic POST response: {topic_post.status_code}')
+
+    message_post = requests.post(post_message_url, data=message_data)
+    print(f'Topic POST response: {message_post.status_code}')
 
 
 # place content of this function into name == main
@@ -91,8 +135,9 @@ def saturday_deletion():
         # del weekly_list[:]
         print(f'It is {current_day}')
     else:
+        write_schedule(current_day)
+        read_schedule()
         set_topic()
-
 
 # get_date()
 fill_schedule()
@@ -106,14 +151,25 @@ week_dict = {
     'Friday': weekly_list[4]
 }
 
-topic = f'The #it-support shift member for the day is {compare_day_schedule().title()}.'
-data = {'token': slack_token,
-        'channel': slack_channel,
-        'topic': topic}
+topic = f'The #it-support shift member for the day is <@{get_shift_member_user_id()}>.'
+topic_data = {
+    'token': slack_token,
+    'channel': slack_channel,
+    'topic': topic
+}
+
+message_data = {
+    'token': slack_token,
+    'channel': get_shift_member_channel_id(),
+    'text': f'<@{get_shift_member_user_id()}> you smell like beans'
+}
 
 # set_topic()
 # saturday_deletion()
 '''
 NEED TO POTENTIALLY WRITE TO A FILE - SEND DMS BASED ON THIS FILE - CLEAR FILE ON SAT, RERUN SCRIPT/REPOPULATE ON SUN
 '''
-write_schedule(current_day)
+# write_schedule(current_day)
+# read_schedule()
+saturday_deletion()
+# message_it_member()
