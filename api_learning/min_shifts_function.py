e958dbe15d422bffe019c8ce73db3be8f9625877
Hayden <hayden.duffy@depop.com>
2020-09-18 17:47:19 +0100

    working min_shifts_function

--- api_learning/min_shifts_function.py
+++ api_learning/min_shifts_function.py
@@ -18,6 +18,7 @@ schedule_exceptions = {
     'Friday': None
 }
 
+
 def max_shifts(remove_from, how_many):
     # Ensures that no IT Member is placed on more than 2 shifts a week
     # https://stackoverflow.com/questions/38599066/removing-some-of-the-duplicates-from-a-list-in-python
@@ -30,32 +31,16 @@ def max_shifts(remove_from, how_many):
 
 def min_shifts(schedule_list, how_many):
     counts = Counter(schedule_list)
-    # for item in schedule_list[:]:
-    #     counts[item] += 1
-    #     if len(schedule_list) > 6 and counts[item] < how_many:
-    #         schedule_list.remove(choice(schedule_list))
-
-    # for member in it_names:
-    #     if len(schedule_list) == 5 and member not in schedule_list:
-    #         schedule_list.remove(choice(schedule_list))
-
     most_common_name = [most_common_member for most_common_member, number in counts.most_common(1)]
-    most_common_number = [number for most_common_member, number in counts.most_common(1)]
-    print(most_common_name)
+
     for member in it_names:
         if len(schedule_list) == 5 and member not in schedule_list:
             schedule_list.remove(most_common_name[0])
 
-    """ Unsure if the next block is necessary
-    # most_common_name = [most_common_member for most_common_member, number in counts.most_common(1)]
-    # most_common_number = [number for most_common_member, number in counts.most_common(1)]
-    # # for member in it_names:
-    # if len(schedule_list) == 10 and most_common_number[0] < how_many:
-    #     schedule_list.remove(most_common_name[0])
-    """
-    # for shift_member in schedule_list[:]:
-    #     if len(schedule_list) == 5 and most_common_value[0] < how_many:
-    #         remove_most_common_value = [schedule_list.remove(most_common_member) for most_common_member, number in counts.most_common(1)]
+    if len(schedule_list) == 5:
+        for most_common_member, number in counts.most_common():
+            if number < how_many:
+                schedule_list.remove(most_common_name[0])
 
 
 def member_day_exclusion(member_to_exclude, day):
